3cc4234a65f7380aec418d20570d1b7c7a567c6a
Aman Karki <sgtest.one@gmail.com>
2024-11-05 17:16:29 -0400

    Added makeBoard func

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,19 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> board = new ArrayList<>(9);
         for (int i = 0; i < 9; i++){
-            //ArrayList<Integer> row = new ArrayList<>(9);
             ArrayList<Integer> row = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
             board.add(row);
 
         }
-        //ArrayList<ArrayList<Integer>> board2 = new ArrayList<>(9);
-        //ArrayList<Integer> row = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        //board2.add(row);
-        //row = new ArrayList<>(Arrays.asList(2,3,4,5,6,7,8,9,1));
-        //board2.add(row);
 
-        //System.out.println(board2);
+
+
         printBoard(board);
 
-        //System.out.println(board);
-        //System.out.println(board.get(0));
+        ArrayList<ArrayList<Integer>> board2 = makeBoard();
+        printBoard(board2);
+
+
     }
 
     public static void printBoard(ArrayList<ArrayList<Integer>> data){
@@ -47,11 +44,27 @@ public class Main {
         for (int x = 0; x<9; x++){
             System.out.println(stringForm.get(x));
             if (x ==2 || x==5){
-                System.out.println("- - - - - - - - - - -");
+                System.out.println("------+-------+------");
             }
 
 
         }
+
+        }
+        public static ArrayList<ArrayList<Integer>> makeBoard(){
+            //ArrayList<ArrayList<Integer>> boards = new ArrayList<ArrayList<Integer>>();
+            ArrayList<Integer> row1 = new ArrayList<> (Arrays.asList(5,3,4,6,7,8,9,1,2));
+            ArrayList<Integer> row2 = new ArrayList<> (Arrays.asList(6,7,2,1,9,5,3,4,8));
+            ArrayList<Integer> row3 = new ArrayList<> (Arrays.asList(1,9,8,3,4,2,5,6,7));
+            ArrayList<Integer> row4 = new ArrayList<> (Arrays.asList(8,5,9,7,6,1,4,2,3));
+            ArrayList<Integer> row5 = new ArrayList<> (Arrays.asList(4,2,6,8,5,3,7,9,1));
+            ArrayList<Integer> row6 = new ArrayList<> (Arrays.asList(7,1,3,9,2,4,8,5,6));
+            ArrayList<Integer> row7 = new ArrayList<> (Arrays.asList(9,6,1,5,3,7,2,8,4));
+            ArrayList<Integer> row8 = new ArrayList<> (Arrays.asList(2,8,7,4,1,9,6,3,5));
+            ArrayList<Integer> row9 = new ArrayList<> (Arrays.asList(3,4,5,2,8,6,1,7,9));
+            //boards.add(row1);
+            ArrayList<ArrayList<Integer>> boards = new ArrayList<>(Arrays.asList(row1,row2,row3,row4,row5,row6,row7,row8,row9));
+            return boards;
 
         }
 
